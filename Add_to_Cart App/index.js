ccdef7ba8fd7cef22b8d858b2884638dca292c3c
Vedant Vatsa <122670026+VedantVatsa@users.noreply.github.com>
2023-08-03 14:31:05 +0530

    Add files via upload

---
+++ Add_to_Cart App/index.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://realtime-database-ffce9-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(shoppingListInDB, inputValue)
+    
+    inputFieldEl.value = ""
+    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
+})
